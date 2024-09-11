3d1c1f40e031120c92170eb3278ed654b842bbdc
MoKharbo <38759@ma-web.nl>
2024-09-11 17:41:53 +0200

    Update Opdracht1.js

--- Opdracht1.js
+++ Opdracht1.js
@@ -1,6 +1,6 @@
-var a = "Mo Kharboutli"
-console.log(a)
-var b = "17"
-console.warn(b)
-var c = "hugo keizer"
-console.error(c)
+var a = "Mo Kharboutli";
+console.log(a);
+var b = "17";
+console.warn(b);
+var c = "hugo keizer";
+console.error(c);
