4be30e6c4377f4059df35d0897d88d1b71863f07
MoKharbo <38759@ma-web.nl>
2024-09-18 17:07:09 +0200

    Update notgoodatmath.js

--- notgoodatmath.js
+++ notgoodatmath.js
@@ -1,11 +1,10 @@
 const date = new Date();
-
-const dagen = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag',  'Zaterdag'];
+const dagen = ['Zonderdag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
 const dagenNu = dagen[date.getDay()];
 
 
 if (dagenNu == 'Maandag' || dagenNu == 'Dinsdag' || dagenNu == 'Woensdag' || dagenNu == 'Donderdag' || dagenNu == 'Vrijdag') {
-    console.log("je hebt school")
-} else if (dagenNu == 'Zondag' || dagenNu == 'Zaterdag') {
-    console.log("je hebt geen school")
+    console.log("je hebt school");
+} else {
+    console.log("je hebt geen school");
 }
