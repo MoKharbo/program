b6948292179d5c1835b3381d16524c4e242616e5
MoKharbo <38759@ma-web.nl>
2024-10-02 16:37:31 +0200

    Update add.js

--- add.js
+++ add.js
@@ -1,28 +1,23 @@
 function kubus(lengte, hoogte, diepte){
     return lengte * hoogte * diepte
 }
-let kubusInhoud = kubus(10, 5, 10);
+let kubusInhoud = kubus(15, 10, 10);
 console.log(kubusInhoud);
-
-function inhoudkubus(diameter, hoogte1){
+function cilindir(diameter, hoogte1){
     let p2i = Math.PI
-    return diameter * p2i * hoogte1
+    let radius = diameter / 2 ;
+    return Math.pow(radius,2) * p2i * hoogte1
 }
-
-let inhoudK = inhoudkubus(10, 5);
+let inhoudK = cilindir(20, 50);
 console.log(inhoudK); 
-
 function driehoek(lengte2, hoogte2){
-    return Math.pow(hoogte2,2) + Math.pow(lengte2,2)
+    return Math.sqrt(Math.pow(lengte2, 2) + Math.pow(hoogte2, 2));
 }
-
-let d32 = driehoek(10,20);
-console.log(d32)
-
+let mango = driehoek(20,50);
+console.log(mango)
 function getallen(one,two,three,four,five,six,seven){
     let gem = one + two + three + four + five + six + seven
     return gem / 7
 }
-
-let gemm = getallen(2,6,4,3,7,2,5)
+let gemm = getallen(2,6,4,7,7,2,1)
 console.log(gemm)
