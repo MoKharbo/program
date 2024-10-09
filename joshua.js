b46188291902840b5aabf993112d7f92ccb4550b
MoKharbo <38759@ma-web.nl>
2024-10-09 15:35:13 +0200

    Update joshua.js

--- joshua.js
+++ joshua.js
@@ -1,4 +1,5 @@
 for(let i = 1; i <= 50; i++){
-    for(let j = 1; j <= 50; j++)
-    console.log(i * j)
+    for(let j = 1; j <= 50; j++){
+        console.log(i + ' * ' + j + ' = ' + (j * i));
+    }
 }
